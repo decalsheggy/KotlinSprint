22c0678eb98058743735963f44918d95514e5fc0
decalsheggy <drozyukkristina70@gmail.com>
2024-04-03 21:28:11 +0600

    KS-1-3 Commit

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -1,12 +1,15 @@
 package org.example.lesson_1
 
 fun main(){
-    val year = 1961
-    var hour = 9
-    var minute = 7
+    val year = "1961"
+    var hour = "09"
+    var minute = "07"
+
     println("Время взлета: \n$year год \n$hour часов \n$minute минут")
-    hour = 10
-    minute = 55
+
+    hour = "10"
+    minute = "55"
+
     println()
     println("Время посадки: $hour:$minute")
 }
