5efa32f91bce2dc14014f5bbdab29a43db64a824
decalsheggy <drozyukkristina70@gmail.com>
2024-04-05 22:53:47 +0600

    KS-3-3 changed

--- src/main/kotlin/lesson_3/lesson3-task3.kt
+++ src/main/kotlin/lesson_3/lesson3-task3.kt
@@ -3,9 +3,17 @@ package org.example.lesson_3
 fun main() {
     val number = 7
     val arrayOfNumber = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
-    for (i in arrayOfNumber) {
-        println("$number * $i = " + number * i)
-    }
+
+    println ("""$number * ${arrayOfNumber[0]} = ${number * arrayOfNumber[0]}
+        |$number * ${arrayOfNumber[1]} = ${number * arrayOfNumber[1]}
+        |$number * ${arrayOfNumber[2]} = ${number * arrayOfNumber[2]}
+        |$number * ${arrayOfNumber[3]} = ${number * arrayOfNumber[3]}
+        |$number * ${arrayOfNumber[4]} = ${number * arrayOfNumber[4]}
+        |$number * ${arrayOfNumber[5]} = ${number * arrayOfNumber[5]}
+        |$number * ${arrayOfNumber[6]} = ${number * arrayOfNumber[6]}
+        |$number * ${arrayOfNumber[7]} = ${number * arrayOfNumber[7]}
+        |$number * ${arrayOfNumber[8]} = ${number * arrayOfNumber[8]}
+    """.trimMargin())
 }
 /*Пишем программу, которая будет помогать учить таблицу умножения.
 В отдельную переменную будет записываться число (объявить и проинициализировать),
@@ -22,4 +30,5 @@ fun main() {
  – Вместо числа 6 использовать данные из объявленной переменной;
  – Вычислять значения прямо в строке;
  – Вывод всей таблицы нужно сделать в одной функции println(),
- которая вызывается один раз.*/
+ которая вызывается один раз.
+ */
