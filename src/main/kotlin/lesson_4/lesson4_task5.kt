0a854c605e49bd98f731e70426d9071bd5bf70b5
decalsheggy <drozyukkristina70@gmail.com>
2024-04-08 16:36:18 +0600

    KS-4-5 commit

--- src/main/kotlin/lesson_4/lesson4_task5.kt
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -1,12 +1,43 @@
 package org.example.lesson_4
 
+
+const val MIN_PEOPLE = 55
+const val MAX_PEOPLE = 70
+const val MIN_PROVISIONS = 50
+const val IS_FAVORABLE_WEATHER = true
+const val IS_NO_DAMAGE = true
+
 fun main() {
-    val damage = false
-    val people = 55..70
-    val minProvisions = 50//не включительно
-    val favorableWeather = true || false
+    val peopleOnBoard = 60
+    val provision = 51
+
+    if ((IS_NO_DAMAGE) &&
+        (peopleOnBoard >= MIN_PEOPLE && peopleOnBoard <= MAX_PEOPLE) &&
+        (provision > MIN_PROVISIONS)
+    ) {
+        println("Научно-исследовательский корабль может приступить" +
+                " к долгосрочному плаванию: ${true}.")
+    } else if ((peopleOnBoard == MAX_PEOPLE) &&
+        (IS_FAVORABLE_WEATHER == true) &&
+        (provision >= MIN_PROVISIONS)
+    ) {
+        println("Научно-исследовательский корабль может приступить" +
+                " к долгосрочному плаванию: ${true}.")
+    } else {
+        println("Научно-исследовательский корабль может приступить" +
+                " к долгосрочному плаванию: ${false}.")
+    }
 }
-/*Научно-исследовательский корабль может приступить
+           /*val isGoSailing = ((IS_NO_DAMAGE)
+            && (peopleOnBoard >= MIN_PEOPLE
+            && peopleOnBoard <= MAX_PEOPLE)
+            && (provision > MIN_PROVISIONS))
+            || ((!IS_NO_DAMAGE == true) &&
+            (peopleOnBoard == MAX_PEOPLE) &&
+            (IS_FAVORABLE_WEATHER == true) &&
+            (provision >= MIN_PROVISIONS))
+            println(isGoSailing)
+Научно-исследовательский корабль может приступить
 к долгосрочному плаванию при выполнении следующих условий:
 
 - корабль не имеет повреждений;
@@ -31,4 +62,5 @@ fun main() {
 Напиши программу, которая будет определять, может ли корабль отправиться в плавание.
 Программа должна анализировать эти данные и выводить результат в консоль.
 Для анализа должно использоваться единое составное условие,
-состоящее только из логических операторов.*/
+состоящее только из логических операторов.
+*/
