9ea1aa10cf58d5c48ac545cf6fc69b3069df5f40
decalsheggy <drozyukkristina70@gmail.com>
2024-04-19 12:47:00 +0600

    KS-9-5 commit

---
+++ src/main/kotlin/lesson_9/lesson9-task5.kt
@@ -0,0 +1,25 @@
+package org.example.lesson_9
+
+
+fun main() {
+    println("Введите пять названий ингредиентов:")
+
+    val size = 5
+    val ingredients = List(size){
+        readln()
+    }
+    val allIngredients = ingredients
+        .toSet()
+        .toList()
+        .sorted()
+        .joinToString  (", ")
+        .replaceFirstChar { if (it.isLowerCase()) it.titlecase() else it.toString() }
+    println(allIngredients)
+
+}
+/*Напиши программу, которая запрашивает у пользователя пять названий ингредиентов (каждый по отдельности).
+
+- результат должен исключать повторы;
+- список должен выводиться в алфавитном порядке;
+- все элементы списка выведены в одну строку, разделяя слова запятыми;
+- !первый элемент должен быть распечатан с заглавной буквы!.*/
