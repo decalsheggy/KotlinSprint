ea5334947968a72061117e291a3a583c8b0297b6
decalsheggy <drozyukkristina70@gmail.com>
2024-04-20 00:03:21 +0600

    KS-10-3 commit

---
+++ src/main/kotlin/lesson_10/lesson10-task3.kt
@@ -0,0 +1,28 @@
+package org.example.lesson_10
+
+fun main() {
+    println("Сколько символов вам нужно в пароле?")
+    val size = readln().toInt()
+    val password = generatePassword(size)
+
+    println("Ваш пароль: $password")
+}
+fun generatePassword(size: Int):String {
+    val password = StringBuilder()
+    val numbers = 0..9
+    val specialChars = "!\"#\$%&'()*+,-./ "
+
+    for (i in 0 until size) {
+        if (i % 2 == 0) password.append(numbers.random()) else password.append(specialChars.random())
+    }
+    return password.toString()
+}
+
+/*Напиши программу, которая генерирует пароли. Пароль должен состоять из
+последовательно чередующихся цифр и специальных символов. Например, 4#4%2!.
+
+- пользователь сам задает длину пароля;
+- для генерации пароля должна быть отдельная функция, принимающая целочисленное
+значение длины пароля и возвращающая готовый пароль;
+- пароль должен содержать цифры от 0 до 9, специальные символы:
+ !"#$%&'()*+,-./ и пробел (всего 16 спецсимволов).*/
