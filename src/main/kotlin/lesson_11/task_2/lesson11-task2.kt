487108e1abd9ace21edd9523124743ab1732bb30
decalsheggy <drozyukkristina70@gmail.com>
2024-04-24 20:54:42 +0600

    KS-11-2 changed

--- src/main/kotlin/lesson_11/task_2/lesson11-task2.kt
+++ src/main/kotlin/lesson_11/task_2/lesson11-task2.kt
@@ -1,16 +1,52 @@
 package org.example.lesson_11.task_2
 
-fun main(){
+fun main() {
     val user = UserData2(
         "warlock_3000",
         "1a2b3c4",
         "vlkvlkvyb5500",
-        true,
+        "ock9753@mail.ru",
     )
     user.writeBiography()
     user.changePassword()
     user.outputInformation()
+}
 
+class UserData2(
+    val id: String,
+    val login: String,
+    var password: String,
+    val mail: String,
+    var bio: String = ""
+) {
+    fun outputInformation() {
+        val info = """
+        id: $id,
+        login: $login,
+        password: $password,
+        message: $mail,
+        bio: $bio 
+        """.trimIndent()
+
+        println("Обновленная информация о пользователе: \n$info")
+    }
+
+    fun writeBiography() {
+        println("Ваша биография:")
+        bio = readln()
+    }
+
+    fun changePassword() {
+        println("Введите ваш старый пароль:")
+        val enteredPassword = readln()
+
+        if (password == enteredPassword) {
+            println("Введите ваш новый пароль:")
+
+            password = readln()
+            println("Пароль изменен на: $password")
+        } else println("Пароль введен неверно.")
+    }
 }
 /* Скопируй класс User из первой задачи и назови User2. Добавь к классу новое
 необязательное свойство bio и несколько методов:
