5cc57202d1ed265b0de23b908492c469cbc491f0
decalsheggy <drozyukkristina70@gmail.com>
2024-04-28 21:48:42 +0500

    KS-13-5 commit

---
+++ src/main/kotlin/lesson_13/lesson13-task5.kt
@@ -0,0 +1,24 @@
+package org.example.lesson_13
+
+fun main(){
+    println("Введите номер телефона:")
+    val input = readln().toLongOrNull()
+    if (input == null) println("Нужно вводить только цифры.")
+    else {
+        val user = PhoneBookData("L", input, "School")
+        user.outputObjects()
+    }
+}
+
+class PhoneBookData(val name: String, val phoneNumber: Long?, val company: String? = null) {
+    fun outputObjects() {
+        println("Имя: $name, номер телефона: $phoneNumber, место работы: ${company}")
+    }
+}
+/*Скопируй класс из предыдущей задачи (без логики заполнения телефонной книги)
+и измени его название для избегания конфликтов.
+
+Для получения номера программа считывает ввод пользователя с типом String по умолчанию.
+Приведи строку к типу Long. Если пользователь ввел что-либо, кроме цифр — программа падает.
+
+Необходимо воспроизвести ошибку и вывести ее название в консоль.*/
