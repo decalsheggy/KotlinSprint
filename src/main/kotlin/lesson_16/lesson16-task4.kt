7dfc17ee8a9fab07ef88a95482aa88e18ce7beb8
decalsheggy <drozyukkristina70@gmail.com>
2024-05-06 22:52:06 +0500

    KS-16-4 commit

---
+++ src/main/kotlin/lesson_16/lesson16-task4.kt
@@ -0,0 +1,25 @@
+package org.example.lesson_16
+
+fun main() {
+    val order = Order(675634, "В пути на склад")
+    order.changeStatus("Готов к выдаче")
+}
+
+class Order(private val numberOfOrder: Int, val statusOfOrder: String) {
+    private fun changeStatusRequest(newStatus: String) {
+        println("Статус заказа $numberOfOrder: \"$statusOfOrder\" изменить на: \"$newStatus\"")
+    }
+
+    fun changeStatus(newStatus: String) {
+        changeStatusRequest(newStatus)
+    }
+}
+/*Создай класс, описывающий заказ в интернет-магазине.
+В заказе должны быть номер заказа и статус готовности.
+Номер заказа должен быть недоступен для изменения.
+
+Создай метод, который меняет статус заказа.
+Поменять статус заказа можно только отправив заявку менеджеру.
+Имитируй это действие отдельной публичной функцией.
+Она будет принимать новый статус и обращаться
+к внутреннему методу класса для изменения статуса.*/
