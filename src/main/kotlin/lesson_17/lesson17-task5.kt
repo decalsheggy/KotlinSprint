511e129be37ecef800442a520494127626b44b84
decalsheggy <drozyukkristina70@gmail.com>
2024-05-11 17:41:11 +0500

    KS-17-5 changed

--- src/main/kotlin/lesson_17/lesson17-task5.kt
+++ src/main/kotlin/lesson_17/lesson17-task5.kt
@@ -2,28 +2,25 @@ package org.example.lesson_17
 
 fun main() {
     val user = User("aB12345", "bA56789")
-    user.changedLogin = "bA54321"
+    user.login = "bA54321"
 
-    user.currentPassword = "135c796D53e"
-    println("Пароль: ${user.currentPassword}")
+    user.password = "135c796D53e"
+    println("Пароль: ${user.password}")
 }
 
 class User(initialLogin: String, initialPassword: String) {
-    private var login = initialLogin
-    private val password = initialPassword
-
-    var currentPassword: String
-        get() = "*".repeat(password.length)
+    var password: String = initialPassword
+        get() = "*".repeat(field.length)
         set(value) {
-            if (value != password) println("Вы не можете изменить пароль на $value.")
+            if (value != field) println("Вы не можете изменить пароль на $value.")
         }
 
-    var changedLogin: String
-        get() = login
+    var login: String = initialLogin
+        get() = field
         set(value) {
-            if (login != value) {
-                login = value
-                println("Логин изменен на $changedLogin.")
+            if (field != value) {
+                field = value
+                println("Логин изменен на $value.")
             }
         }
 }
