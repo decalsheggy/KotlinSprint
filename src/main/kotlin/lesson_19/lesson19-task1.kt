6dfad92aeb5a8b1c2ea3155b6efc3f667e13393d
decalsheggy <drozyukkristina70@gmail.com>
2024-05-11 11:09:19 +0500

    KS-19-1 commit

---
+++ src/main/kotlin/lesson_19/lesson19-task1.kt
@@ -0,0 +1,18 @@
+package org.example.lesson_19
+
+fun main() {
+    val fishes: List<Fishes> = listOf(Fishes.GUPPY, Fishes.ANGELFISH, Fishes.GOLDFISH, Fishes.SIAMESE_FISH)
+
+    println("Доступные рыбы для добавления в аквариум: ${fishes.joinToString { it.nameOfFish }}.")
+}
+
+enum class Fishes(val nameOfFish: String) {
+    GUPPY("гуппи"),
+    ANGELFISH("скалярия"),
+    GOLDFISH("золотая рыбка"),
+    SIAMESE_FISH("петушок"),
+}
+
+/*В симуляторе аквариума есть 4 вида рыб. Гуппи (guppy), скалярия (angelfish), золотая рыбка (Goldfish),
+петушок (Siamese fighting fish). Напиши enum класс, перечисляющий этих рыб.
+Сообщи пользователю, каких рыб он может добавить в свой аквариум, распечатав список в консоль.*/
