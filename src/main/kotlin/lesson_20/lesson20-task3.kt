15ec82c7e0f9da68b6be0281bf6baa30b1a4bf98
decalsheggy <drozyukkristina70@gmail.com>
2024-05-12 23:27:31 +0500

    KS-20-3 commit

---
+++ src/main/kotlin/lesson_20/lesson20-task3.kt
@@ -0,0 +1,20 @@
+package org.example.lesson_20
+
+fun main() {
+    val player1 = Player("Игрок1", false)
+    val player2 = Player("Игрок2", true)
+
+    val checkTheKey: (Player) -> Unit = {
+        if (it.key) println("${it.name} открыл дверь.")
+        else println("${it.name} не может войти. Дверь заперта.")
+    }
+    checkTheKey(player1)
+    checkTheKey(player2)
+}
+
+class Player(val name: String, var key: Boolean)
+
+/*В игре есть запертая дверь. Игрок должен найти ключ. Нужно написать лямбда-функцию, которая будет проверять,
+есть ли у игрока ключ. Если ключ есть, нужно вывести сообщение, что игрок дверь открыл, иначе – что дверь заперта.
+
+Для проверки работы функции нужно написать класс игрока, в котором будет храниться информация о наличии ключа.*/
