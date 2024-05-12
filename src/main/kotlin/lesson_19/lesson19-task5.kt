2b154f665bd8f7c843d62751357b20234de7f864
decalsheggy <drozyukkristina70@gmail.com>
2024-05-12 21:00:34 +0500

    KS-19-5 changed

--- src/main/kotlin/lesson_19/lesson19-task5.kt
+++ src/main/kotlin/lesson_19/lesson19-task5.kt
@@ -1,5 +1,6 @@
 package org.example.lesson_19
 
+import java.util.NoSuchElementException
 import java.util.Scanner
 
 fun main() {
@@ -7,26 +8,28 @@ fun main() {
     val scanner = Scanner(System.`in`)
 
     println(
-        "Введите ваше имя и пол (${Gender.values().joinToString { it.name }})\n" +
+        "Введите ваше имя и пол (${Gender.values().joinToString { it.person }})\n" +
                 "Ввод текста должен быть таким: Имя пол"
     )
 
     while (people.size < 5) {
-        val input = scanner.nextLine()
-        val parts = input.split(" ")
-        if (parts.size == 2) {
-            val name = parts[0]
-            val genderInput = parts[1]
-            val gender = Gender.fromPerson(genderInput)
-            if (gender != null) {
+        try {
+            val input = scanner.nextLine().trim()
+            val parts = input.split(" ")
+            if (parts.size == 2) {
+                val name = parts[0]
+                val genderInput = parts[1]
+                val gender = Gender.fromPerson(genderInput)
                 val person = Person(name, gender)
                 people.add(person)
-            } else println("Ошибка в написании пола.")
-        } else println("Допущена ошибка в написании.")
+            } else println("Допущена ошибка в написании. Убедитесь, что вы ввели имя и пол через один пробел.")
+        } catch (e: NoSuchElementException) {
+            println("Ошибка в написании пола. Допустимые значения: женщина, мужчина.")
+        }
     }
     println("Данные картотеки:")
     people.forEach {
-        println("${it.nameOfPerson}(${it.genderOfPerson?.person})")
+        println("${it.nameOfPerson}(${it.genderOfPerson.person})")
     }
 }
 
@@ -35,13 +38,13 @@ enum class Gender(val person: String) {
     MAN("мужчина");
 
     companion object {
-        fun fromPerson(person: String): Gender? {
-            return values().firstOrNull { it.person.equals(person, true) }
+        fun fromPerson(person: String): Gender {
+            return values().first { it.person.equals(person, true) }
         }
     }
 }
 
-class Person(val nameOfPerson: String, val genderOfPerson: Gender?)
+class Person(val nameOfPerson: String, val genderOfPerson: Gender)
 
 /*Разрабатываем приложение “Картотека”.
 Пользователь вводит имя и пол человека. Данные картотеки хранятся в списке.
