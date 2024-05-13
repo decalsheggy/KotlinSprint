6dc5387bec7708cde70dbc42558ece3769b8abba
decalsheggy <drozyukkristina70@gmail.com>
2024-05-14 00:32:29 +0500

    KS-20-3 changed

--- src/main/kotlin/lesson_20/lesson20-task3.kt
+++ src/main/kotlin/lesson_20/lesson20-task3.kt
@@ -4,12 +4,12 @@ fun main() {
     val player1 = Player("Игрок1", false)
     val player2 = Player("Игрок2", true)
 
-    val checkTheKey: (Player) -> Unit = {
+    val hasKey: (Player) -> Unit = {
         if (it.key) println("${it.name} открыл дверь.")
         else println("${it.name} не может войти. Дверь заперта.")
     }
-    checkTheKey(player1)
-    checkTheKey(player2)
+    hasKey(player1)
+    hasKey(player2)
 }
 
 class Player(val name: String, var key: Boolean)
