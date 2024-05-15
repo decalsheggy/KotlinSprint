8786762919c3fa0586af0b6a6be06e0d2cf1a8ae
decalsheggy <drozyukkristina70@gmail.com>
2024-05-15 20:50:12 +0500

    KS-21-4 commit

--- src/main/kotlin/lesson_21/lesson21-task4.kt
+++ src/main/kotlin/lesson_21/lesson21-task4.kt
@@ -3,21 +3,12 @@ package org.example.lesson_21
 import java.io.File
 
 fun main() {
-    val file = File("text")
-    file.writeWordToFile("word")
+    val file = File("text.txt")
+    file.writeWordToFile("Слово")
 }
 
 fun File.writeWordToFile(word: String) {
-    createNewFile()
-    writeText(word)
-    println(word.lowercase() + this)
+    val currentContent = if (this.exists()) this.readText() else ""
+    this.writeText(word.lowercase() + "\n" + currentContent)
+    println("Записанное слово: ${word.lowercase()}.")
 }
-/*val message = "some message for file"
-В курсовой работе мы разрабатываем телеграм-бота на Kotlin,
-который помогает пользователям учить иностранные слова.
-Создай extension функцию для класса File из стандартной библиотеки,
-которая будет записывать переданное ей слово в файл.
-
-- дополнительные материалы про файлы есть в 1-2 этапах курсовой;
-- функция-расширение должна записывать текст в нижнем регистре;
-- функция-расширение должна записывать текст в начало файла.*/
