27b38af69e240974c83dd0d277a462e8953c5ea4
decalsheggy <drozyukkristina70@gmail.com>
2024-05-16 11:26:13 +0500

    KS-21-5 commit

--- src/main/kotlin/lesson_21/lesson21-task5.kt
+++ src/main/kotlin/lesson_21/lesson21-task5.kt
@@ -18,12 +18,5 @@ fun main() {
 fun Map<String, Int>.maxCategory(): String? {
     val maxValue = this.maxByOrNull { it.value }?.value
     val maxKeys = this.filter { it.value == maxValue }.keys.toList()
-
     return if (maxKeys.isNotEmpty()) maxKeys[Random().nextInt(maxKeys.size)] else null
 }
-/*Игра имеет систему прокачки персонажей по различным навыкам. Опыт в каждом навыке хранится в виде пары
-ключ-значение, где ключ — это название навыка (String), а значение — количество очков опыта (Int).
-
-Создай extension функцию для интерфейса Map с названием maxCategory, которая будет возвращать название навыка,
-в котором у игрока наибольшее количество очков опыта.
-Если два или более навыка имеют одинаковое максимальное значение, функция должна возвращать любой из них.*/
