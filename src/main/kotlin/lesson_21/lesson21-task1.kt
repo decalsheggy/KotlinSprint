7954b900810c5adc5372253610733fac25395172
decalsheggy <drozyukkristina70@gmail.com>
2024-05-16 20:28:51 +0500

    KS-21-1 changed

--- src/main/kotlin/lesson_21/lesson21-task1.kt
+++ src/main/kotlin/lesson_21/lesson21-task1.kt
@@ -1,11 +1,10 @@
 package org.example.lesson_21
 
 fun main() {
-    val string = "Сколько гласных в этой строке? - How many vowels are there in this line?"
-    println(string.vowelCount())
+    val phrase = "Сколько гласных в этой строке? - How many vowels are there in this line?"
+    println(phrase.vowelCount())
 }
 
 fun String.vowelCount(): Int {
-    val vowels = "аеёиоуыэюяАЕЁИОУЫЭЮЯaeiouAEIOU"
-    return this.count { it in vowels }
+    return count { it in "аеёиоуыэюяАЕЁИОУЫЭЮЯaeiouAEIOU" }
 }
