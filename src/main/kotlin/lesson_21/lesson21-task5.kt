af9e248221896de1b759611459857b3b7e7902a9
decalsheggy <drozyukkristina70@gmail.com>
2024-05-17 14:16:59 +0500

    KS-21-5 changed

--- src/main/kotlin/lesson_21/lesson21-task5.kt
+++ src/main/kotlin/lesson_21/lesson21-task5.kt
@@ -1,22 +1,17 @@
 package org.example.lesson_21
 
-import java.util.Random
-
 fun main() {
     val categories =
         mapOf(
             "Ловкость" to 10,
             "Интеллект" to 14,
-            "Мудрость" to 14,
-            "Харизма" to 19,
+            "Мудрость" to 17,
+            "Харизма" to 17,
             "Сила" to 10,
             "Телосложение" to 13,
         )
     println(categories.maxCategory())
 }
 
-fun Map<String, Int>.maxCategory(): String? {
-    val maxValue = this.maxByOrNull { it.value }?.value
-    val maxKeys = this.filter { it.value == maxValue }.keys.toList()
-    return if (maxKeys.isNotEmpty()) maxKeys[Random().nextInt(maxKeys.size)] else null
-}
+fun Map<String, Int>.maxCategory(): String? = maxByOrNull { it.value }?.key
+
