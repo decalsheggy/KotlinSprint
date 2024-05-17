2f8c9cb3fa204769d01e510ada7c029a993a7cc5
decalsheggy <drozyukkristina70@gmail.com>
2024-05-17 15:44:41 +0500

    KS-22-5 commit

--- src/main/kotlin/lesson_22/lesson22-task5.kt
+++ src/main/kotlin/lesson_22/lesson22-task5.kt
@@ -9,7 +9,7 @@ fun main() {
             |Альфа Центавра A - её масса чуть больше солнечной, и она является ярчайшей звездой в системе; 
             |Альфа Центавра B - эта звезда меньше и холоднее, чем Альфа Центавра A; 
             |Проксима Центавра - это красный карлик, значительно меньше и холоднее основных звезд системы.""".trimMargin(),
-        LocalDateTime.of(2024, 5, 17, 15, 30),
+        null,
         4.367
     )
     val (nameOfPlaceOrEvent, descriptionOfPlaceOrEvent,timeOfEvent,distance) = place
@@ -17,24 +17,12 @@ fun main() {
     println("Название места: $nameOfPlaceOrEvent")
     println("Описание места: $descriptionOfPlaceOrEvent")
     println("Время события: $timeOfEvent")
-    println("Расстояние (в световых годах): $distance")
+    println("Расстояние : $distance световых лет")
 }
 
 data class GalacticGuide(
     val nameOfPlaceOrEvent: String,
     val descriptionOfPlaceOrEvent: String,
-    val timeOfEvent: LocalDateTime,
+    val timeOfEvent: LocalDateTime?,
     val distance: Double,
 )
-/*В Путеводитель для путешествующих автостопом по Галактике добавляется
-информация о месте или событии в галактике в виде объектов.
-
-Создай data class GalacticGuide cо свойствами:
-
-- название места или события;
-- описание места или события;
-- дата и время события;
-- расстояние места или события от Земли в световых годах.
-
-Создай объект с информацией о звездной системе Alpha Centauri.
-Выведи информацию о месте, используя компонентные функции data class.*/
