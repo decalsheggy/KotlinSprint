87f6abc2110886f28e2fd6c6280e5799e0908edf
decalsheggy <drozyukkristina70@gmail.com>
2024-05-17 20:52:19 +0500

    KS-22-4 changed

--- src/main/kotlin/lesson_22/lesson22-task4.kt
+++ src/main/kotlin/lesson_22/lesson22-task4.kt
@@ -6,26 +6,25 @@ fun main() {
 }
 
 class MainScreenViewModel {
+    data class MainScreenState(
+        val data: String,
+        val isLoading: Boolean = false,
+    )
+
     private var mainScreenState: MainScreenState = MainScreenState("Данные отсутствуют")
-
-    fun loadData() {
-        println("Загрузка данных:")
-        mainScreenState = mainScreenState.copy(isLoading = true)
-        printState()
-
-        Thread.sleep(2000)
-
-        val fetchedData = "Загруженные данные с сервера"
-        mainScreenState = mainScreenState.copy(data = fetchedData, isLoading = false)
-        printState()
-    }
 
     private fun printState() {
         println("Текущее состояние: data = ${mainScreenState.data}, isLoading = ${mainScreenState.isLoading}")
     }
+
+    fun loadData() {
+        println("Загрузка данных..")
+        mainScreenState = mainScreenState.copy(isLoading = true)
+        printState()
+        Thread.sleep(2000)
+        val fetchedData = "Загруженные данные с сервера"
+        mainScreenState = mainScreenState.copy(data = fetchedData, isLoading = false)
+        printState()
+    }
 }
 
-data class MainScreenState(
-    val data: String,
-    val isLoading: Boolean = false,
-)
